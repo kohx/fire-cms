d6bcd809fd22087e0135ec42b2ec342db610c4d2
KOHX\kohei <kohei.0728@gmail.com>
2018-11-03 18:55:41 +0900

    backend settings fixed

--- functions/events/https/backendDir/contents/base.js
+++ functions/events/https/backendDir/contents/base.js
@@ -5,38 +5,51 @@ export class Base {
         this.notice = document.querySelector('.notice')
         this.noticeTitle = document.querySelector('.notice-title')
         this.noticeMessages = document.querySelector('.notice-messages')
+
+        /* notice */
+        this.noticeClose = document.querySelector('.notice-close')
+        this.noticeClose.addEventListener('click', event => {
+            this.closeNotice()
+        })
     }
 
     static init() {
         return new Base()
     }
 
-    setNotice(type, messages, timeout = 6000) {
+    /* nitice */
+    setNotice(type, messages, timeout = 4000) {
         this.clearNotice()
-        this.notice.classList.add(`__${type}`)
         this.noticeTitle.textContent = type.toUpperCase()
         Object.keys(messages).forEach(key => {
             const list = document.createElement('li');
             list.textContent = messages[key]
             this.noticeMessages.insertAdjacentElement('beforeend', list)
         });
-
+        this.notice.classList.add(`__${type}`, '_active')
         setTimeout(_ => {
-            this.clearNotice(1000)
+            this.closeNotice()
         }, timeout);
     }
 
-    clearNotice(delay) {
+    clearNotice() {
         this.notice.classList.remove('__info')
         this.notice.classList.remove('__success')
         this.notice.classList.remove('__warning')
         this.notice.classList.remove('__error')
-        setTimeout(_ => {
-            this.noticeTitle.textContent = null
-            this.noticeMessages.textContent = null
-        }, delay);
+        this.noticeTitle.textContent = null
+        this.noticeMessages.textContent = null
     }
 
+    closeNotice(){
+        this.notice.classList.remove('_active')
+        setTimeout(_ => {
+            this.clearNotice()
+        }, 10000);
+    }
+
+
+    /* fetche */
     fetchPost(url, body = {}) {
 
         const headers = {
@@ -47,13 +60,13 @@ export class Base {
 
         return new Promise((resolve, reject) => {
             fetch(url, {
-                method: 'post',
-                mode: 'cors',
-                credentials: 'include',
-                cache: 'no-cache',
-                headers: headers,
-                body: JSON.stringify(body)
-            })
+                    method: 'post',
+                    mode: 'cors',
+                    credentials: 'include',
+                    cache: 'no-cache',
+                    headers: headers,
+                    body: JSON.stringify(body)
+                })
                 .then(data => {
                     return data.json()
                 })
